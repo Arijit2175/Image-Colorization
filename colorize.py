e1102215e77b28dfa2bc33fa429d0998c9cd228b
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-21 09:58:14 +0530

    reading and preprocessing

--- colorize.py
+++ colorize.py
@@ -9,3 +9,7 @@ pts_file = "Models/pts_in_hull.npy"
 net = dnn.readNetFromCaffe(proto_file, model_file)
 pts = np.load(pts_file)
 
+img = cv2.imread("Input/image.jpg")
+scaled = img.astype("float32") / 255.0
+lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
+
