0718cf1fbfa38ebf4c2e312d992a0654622cd180
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-21 09:54:13 +0530

    model file paths

--- colorize.py
+++ colorize.py
@@ -2,3 +2,7 @@ import numpy as np
 import cv2
 from cv2 import dnn
 
+proto_file = "Models/colorization_deploy_v2.prototxt"
+model_file = "Models/colorization_release_v2.caffemodel"
+pts_file = "Models/pts_in_hull.npy"
+
