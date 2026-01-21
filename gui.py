6d64ba066d63021dc99c76cef505a3546d1cdfeb
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-21 10:10:54 +0530

    defined the model paths

--- gui.py
+++ gui.py
@@ -7,3 +7,14 @@ from cv2 import dnn
 import os
 from threading import Thread
 
+class ImageColorizerGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Image Colorization Tool")
+        self.root.geometry("1200x700")
+        
+        self.proto_file = "Models/colorization_deploy_v2.prototxt"
+        self.model_file = "Models/colorization_release_v2.caffemodel"
+        self.pts_file = "Models/pts_in_hull.npy"
+
+        
