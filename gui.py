2db5b95121626bd9474d471557417a910deeb7e9
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-21 10:10:20 +0530

    added imports

---
+++ gui.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
+import numpy as np
+import cv2
+from cv2 import dnn
+import os
+from threading import Thread
+
